d421ceb177881d949fdab80e440309d28e3a2c73
mh72156@gmail.com <mh72156>
2020-01-25 18:42:10 +0600

    üp

---
+++ lib/utils/widget/order_sheet.dart
@@ -0,0 +1,196 @@
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
+import 'cookie_button.dart';
+
+enum PaymentType { cardPayment, googlePay, applePay }
+final int cookieAmount = 200;
+
+String getCookieAmount() => (cookieAmount / 100).toStringAsFixed(2);
+
+class OrderSheet extends StatelessWidget {
+  final bool googlePayEnabled;
+  final bool applePayEnabled;
+  final String balance;
+  OrderSheet({this.balance,this.googlePayEnabled, this.applePayEnabled});
+
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: const Radius.circular(20.0),
+                topRight: const Radius.circular(20.0))),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 10, top: 10),
+                child: _title(context),
+              ),
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                    minWidth: MediaQuery.of(context).size.width,
+                    minHeight: 300,
+                    maxHeight: MediaQuery.of(context).size.height,
+                    maxWidth: MediaQuery.of(context).size.width),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      _ShippingInformation(),
+                      _LineDivider(),
+                      _PaymentTotal(double.parse(balance)),
+                      _LineDivider(),
+                      _RefundInformation(),
+                      _payButtons(context),
+                    ]),
+              ),
+            ]),
+      );
+
+  Widget _title(context) =>
+      Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+        Container(
+            child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.close),
+                color: Colors.black)),
+        Container(
+          child: Expanded(
+            child: Text(
+              "Place your payment",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,color: Colors.deepPurple),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        Padding(padding: EdgeInsets.only(right: 56)),
+      ]);
+
+  Widget _payButtons(context) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          CookieButton(
+            text: "Pay with card",
+            onPressed: () {
+              Navigator.pop(context, PaymentType.cardPayment);
+            },
+          ),
+          Container(
+            height: 64,
+            width: MediaQuery.of(context).size.width * .4,
+            child: RaisedButton(
+              onPressed: googlePayEnabled || applePayEnabled
+                  ? () {
+                      if (Platform.isAndroid) {
+                        Navigator.pop(context, PaymentType.googlePay);
+                      } else if (Platform.isIOS) {
+                        Navigator.pop(context, PaymentType.applePay);
+                      }
+                    }
+                  : null,
+              child: Image(
+                  image: (Theme.of(context).platform == TargetPlatform.iOS)
+                      ? AssetImage("images/applePayLogo.png")
+                      : AssetImage("images/googlePayLogo.png")),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+              color: Colors.deepPurple,
+            ),
+          ),
+        ],
+      );
+}
+
+class _ShippingInformation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(left: 30)),
+          Text(
+            "Ship to",
+            style:Theme.of(context).textTheme.title,
+          ),
+          Padding(padding: EdgeInsets.only(left: 30)),
+          Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Mamun Hossain",
+                  style:Theme.of(context).textTheme.title.copyWith(fontSize: 16.0,fontWeight: FontWeight.w500),
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 6),
+                ),
+                Text(
+                  "Dhaka\nBangladesh",
+                  style:Theme.of(context).textTheme.display1,
+                ),
+              ]),
+        ],
+      );
+}
+
+class _LineDivider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+      margin: EdgeInsets.only(left: 30, right: 30),
+      child: Divider(
+        height: 1,
+        color: Colors.deepPurpleAccent,
+      ));
+}
+
+class _PaymentTotal extends StatelessWidget {
+
+  final double balance;
+
+
+  _PaymentTotal(this.balance);
+
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(left: 30)),
+          Text(
+            "Total",
+            style:Theme.of(context).textTheme.title,
+          ),
+          Padding(padding: EdgeInsets.only(right: 47)),
+          Text(
+            "\$${balance/100}",
+            style:Theme.of(context).textTheme.title,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      );
+}
+
+class _RefundInformation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => FittedBox(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+              width: MediaQuery.of(context).size.width - 60,
+              child: Text(
+                "You can refund this transaction through your Square dashboard, go to squareup.com/dashboard.",
+                style:Theme.of(context).textTheme.display1,
+              ),
+            ),
+          ],
+        ),
+      );
+}
